a29ec2d414d4a8dbd654952138c7d59e01997854
mr02june <mr02june@gmail.com>
2024-09-01 19:13:13 +0000

    updating android configuration

--- android/app/src/main/kotlin/com/example/inshort_clone/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/inshort_clone/MainActivity.kt
@@ -1,18 +1,5 @@
-package com.example.inshort_clone
+package com.example.ppp
 
-import android.os.Bundle
 import io.flutter.embedding.android.FlutterActivity
-import io.flutter.embedding.engine.FlutterEngine
-import io.flutter.plugins.GeneratedPluginRegistrant
 
-class MainActivity: FlutterActivity() {
-    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
-        super.configureFlutterEngine(flutterEngine)
-        GeneratedPluginRegistrant.registerWith(flutterEngine)
-    }
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        // Additional setup if needed
-    }
-}
+class MainActivity: FlutterActivity()
